484d20ac88798cd06de9f6c1ea3195679c03e80f
Dominic Wong <dominic@hailocab.com>
2013-08-05 16:08:19 -0400

    Error on Lock() when part of path has already been created

--- zk/lock_test.go
+++ zk/lock_test.go
@@ -55,3 +55,27 @@ func TestLock(t *testing.T) {
 		t.Fatalf("Expected 3 instead of %d", x)
 	}
 }
+
+func TestMultiLevelLock(t *testing.T) {
+	zk, _, err := Connect([]string{"127.0.0.1:2182"}, time.Second*15)
+	if err != nil {
+		t.Fatalf("Connect returned error: %+v", err)
+	}
+	defer zk.Close()
+
+	acls := WorldACL(PermAll)
+	path := "/test/some-foobar/deep-123"
+	if p, err := zk.Create(path, []byte{1, 2, 3, 4}, 0, WorldACL(PermAll)); err != nil {
+		//t.Fatalf("Create returned error: %+v", err)
+	} else if p != path {
+		t.Fatalf("Create returned different path '%s' != '%s'", p, path)
+	}
+	l := NewLock(zk, "/test/some-foobar/deep-123/path/again", acls)
+	defer zk.Delete("/test/some-foobar/deep-123/path/again", 0)
+	if err := l.Lock(); err != nil {
+		t.Fatal(err)
+	}
+	if err := l.Unlock(); err != nil {
+		t.Fatal(err)
+	}
+}
