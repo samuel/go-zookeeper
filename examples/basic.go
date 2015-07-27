bbc8789055d64b168c5ca9e973c029998d332a7e
cyflhn <cyflhn@163.com>
2015-07-27 17:54:32 +0800

    add connection config when connect to zk

--- examples/basic.go
+++ examples/basic.go
@@ -4,19 +4,27 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/samuel/go-zookeeper/zk"
+	"../zk"
 )
 
 func main() {
-	c, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second) //*10)
+
+	servers := []string{"192.168.28.191:2181"}
+	_, _, err := ConnectWithTimeout(servers, 2*time.Second, 5*time.Second)
 	if err != nil {
-		panic(err)
+		fmt.Println("success")
 	}
-	children, stat, ch, err := c.ChildrenW("/")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v %+v\n", children, stat)
-	e := <-ch
-	fmt.Printf("%+v\n", e)
+	/*
+		c, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second) //*10)
+		if err != nil {
+			panic(err)
+		}
+		children, stat, ch, err := c.ChildrenW("/")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v %+v\n", children, stat)
+		e := <-ch
+		fmt.Printf("%+v\n", e)
+	*/
 }
