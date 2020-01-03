b9c408170530f8ffc5deb6a06482d9f19069005f
linalinn <10799908+linalinn@users.noreply.github.com>
2020-01-03 14:42:05 +0100

    is it working?

--- zk/server_java_test.go
+++ zk/server_java_test.go
@@ -50,6 +50,17 @@ func NewIntegrationTestServer(t *testing.T, configPath string, stdout, stderr io
 	// password is 'test'
 	superString := `SERVER_JVMFLAGS=-Dzookeeper.DigestAuthenticationProvider.superDigest=super:D/InIHSb7yEEbrWz8b9l71RjZJU=`
 
+	if os.Getenv("tls") == "true" {
+		superString = `SERVER_JVMFLAGS=-Dzookeeper.DigestAuthenticationProvider.superDigest=super:D/InIHSb7yEEbrWz8b9l71RjZJU=
+        -Dzookeeper.serverCnxnFactory=org.apache.zookeeper.server.NettyServerCnxnFactory
+		-Dzookeeper.ssl.keyStore.location=/tmp/certs/zookeeper.server.keystore.jks
+		-Dzookeeper.ssl.keyStore.password=password
+		-Dzookeeper.ssl.trustStore.location=/tmp/certs/zookeeper.server.truststore.jks
+		-Dzookeeper.ssl.trustStore.password=password`
+	} else {
+		fmt.Println("No TÖS")
+	}
+
 	return &server{
 		cmdString: filepath.Join(zkPath, "zkServer.sh"),
 		cmdArgs:   []string{"start-foreground", configPath},
@@ -76,10 +87,12 @@ func (srv *server) Stop() error {
 }
 
 type ServerConfigServer struct {
-	ID                 int
-	Host               string
-	PeerPort           int
-	LeaderElectionPort int
+	ID                       int
+	Host                     string
+	PeerPort                 int
+	LeaderElectionPort       int
+	PeerPortSecure           int
+	LeaderElectionPortSecure int
 }
 
 type ServerConfig struct {
@@ -91,6 +104,7 @@ type ServerConfig struct {
 	AutoPurgeSnapRetainCount int    // Number of snapshots to retain in dataDir
 	AutoPurgePurgeInterval   int    // Purge task internal in hours (0 to disable auto purge)
 	Servers                  []ServerConfigServer
+	ClientPortSecure         int
 }
 
 func (sc ServerConfig) Marshall(w io.Writer) error {
@@ -117,6 +131,14 @@ func (sc ServerConfig) Marshall(w io.Writer) error {
 		sc.ClientPort = DefaultPort
 	}
 	fmt.Fprintf(w, "clientPort=%d\n", sc.ClientPort)
+
+	if os.Getenv("tls") == "true" {
+		fmt.Println("tls")
+		fmt.Fprintf(w, "secureClientPort=%d\n", sc.ClientPortSecure)
+	} else {
+		fmt.Println("No tls")
+	}
+
 	if sc.AutoPurgePurgeInterval > 0 {
 		if sc.AutoPurgeSnapRetainCount <= 0 {
 			sc.AutoPurgeSnapRetainCount = DefaultServerAutoPurgeSnapRetainCount
