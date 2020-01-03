29b93d250da41123f704dc862976624ae7d593dd
linalinn <10799908+linalinn@users.noreply.github.com>
2020-01-03 09:50:22 +0100

    fix if condition for contorting tls

--- zk/server_java_test.go
+++ zk/server_java_test.go
@@ -50,7 +50,7 @@ func NewIntegrationTestServer(t *testing.T, configPath string, stdout, stderr io
 	// password is 'test'
 	superString := `SERVER_JVMFLAGS=-Dzookeeper.DigestAuthenticationProvider.superDigest=super:D/InIHSb7yEEbrWz8b9l71RjZJU=`
 
-	if os.Getenv("tls") != "true" {
+	if os.Getenv("tls") == "true" {
 		superString = `SERVER_JVMFLAGS=
 		-Dzookeeper.serverCnxnFactory=org.apache.zookeeper.server.NettyServerCnxnFactory
 		-Dzookeeper.ssl.keyStore.location=/tmp/certs/zookeeper.server.keystore.jks
