f42c1facc2730bab51dfeb80a5b2775c7cad48b4
nikhilbhide <nikhilbhide15@gmail.com>
2020-02-08 11:25:37 +0530

    added a validation to check whether lock exists in case of network partition failure

--- zk/lock.go
+++ zk/lock.go
@@ -12,6 +12,7 @@ var (
 	ErrDeadlock = errors.New("zk: trying to acquire a lock twice")
 	// ErrNotLocked is returned by Unlock when trying to release a lock that has not first be acquired.
 	ErrNotLocked = errors.New("zk: not locked")
+
 )
 
 // Lock is a mutual exclusion lock.
@@ -22,6 +23,8 @@ type Lock struct {
 	lockPath string
 	seq      int
 }
+
+var mapEphermeralSequenceLockPath = make(map[string]string)
 
 // NewLock creates a new lock instance using the provided connection, path, and acl.
 // The path must be a node that is only used by this lock. A lock instances starts
@@ -43,6 +46,12 @@ func parseSeq(path string) (int, error) {
 // is acquired or an error occurs. If this instance already has the lock
 // then ErrDeadlock is returned.
 func (l *Lock) Lock() error {
+	if(mapEphermeralSequenceLockPath[l.path]!="") {
+		if(lockExists(l.c,l.path,mapEphermeralSequenceLockPath[l.path])) {
+			return nil
+		}
+	}
+
 	if l.lockPath != "" {
 		return ErrDeadlock
 	}
@@ -53,6 +62,7 @@ func (l *Lock) Lock() error {
 	var err error
 	for i := 0; i < 3; i++ {
 		path, err = l.c.CreateProtectedEphemeralSequential(prefix, []byte{}, l.acl)
+		mapEphermeralSequenceLockPath[l.path] = path
 		if err == ErrNoNode {
 			// Create parent node.
 			parts := strings.Split(l.path, "/")
@@ -146,5 +156,46 @@ func (l *Lock) Unlock() error {
 	}
 	l.lockPath = ""
 	l.seq = 0
+	delete(mapEphermeralSequenceLockPath,l.path)
 	return nil
+}
+
+//Checks whether lock got created and response was lost because of network partition failure.
+//It queries zookeeper and scans existing sequential ephemeral znodes under the parent path
+//It finds out that previously requested sequence number corresponds to child having lowest sequence number
+func lockExists(c *Conn, rootPath string, znodePath string) bool {
+	seq, err := parseSeq(znodePath)
+	if err != nil {
+		return false
+	}
+
+	//scans the existing znodes if there are any
+	for {
+		children, _, err := c.Children(rootPath)
+		if err != nil {
+			return false
+		}
+
+		lowestSeq := seq
+		prevSeq := -1
+		for _, p := range children {
+			s, err := parseSeq(p)
+			if err != nil {
+				return false
+			}
+			if s < lowestSeq {
+				lowestSeq = s
+			}
+			if s < seq && s > prevSeq {
+				prevSeq = s
+			}
+		}
+
+		if seq == lowestSeq {
+			// Acquired the lock
+			return true
+		} else {
+			return false
+		}
+	}
 }
